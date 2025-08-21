c7c38fd4feea1b5e0d2f46e3ceffc270623b55e4
jbz <suicide.leaf@gmail.com>
2025-08-21 23:18:26 +0200

    Added docstring comments for moves

--- models/moves.py
+++ models/moves.py
@@ -1,14 +1,40 @@
+"""
+Module defining the Move enumeration and validation utility for move commands.
+"""
+
 # models/moves.py
 
 from enum import Enum
 
 class Move(Enum):
+    """
+    Enumeration representing possible move commands for a bot.
+
+    Members:
+        R (int): Turn right.
+        L (int): Turn left.
+        F (int): Move forward.
+    """
     R = 1
     L = 2
     F = 3
 
 
 def validate_moves(moves_input: str):
+    """
+    Validate and convert a string of move commands into a list of Move enums.
+
+    Args:
+        moves_input (str): A string representing a sequence of move commands,
+                           e.g. "RFLF".
+
+    Returns:
+        list[Move]: List of Move enum members corresponding to the input commands.
+
+    Raises:
+        KeyError: If any character in moves_input does not correspond to a Move enum member.
+    """
+
     moves = []
     for move_input in moves_input:
         moves.append(Move[move_input])
